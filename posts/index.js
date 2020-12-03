b9d616816b0f3c21cd1f5fb7190f9958815bc43f
bishkou <chedyhammami@yahoo.com>
2020-12-03 09:44:27 +0100

    skaffold sync

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -41,5 +41,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v55")
   console.log('Listening on 4000');
 });
